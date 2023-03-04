1fa19ac5e5a86429bd0bd4d2e6f01d8009ea4b2c
helight <helightdev@gmail.com>
2023-03-04 12:03:28 +0100

    refactor: move annotations to converter

--- packages/dogs/lib/src/converter.dart
+++ packages/dogs/lib/src/converter.dart
@@ -27,3 +27,64 @@ abstract class DogConverter<T> extends TypeCapture<T> {
   DogGraphValue convertToGraph(T value, DogEngine engine);
   T convertFromGraph(DogGraphValue value, DogEngine engine);
 }
+
+/// Marks a class or enum as serializable.
+/// The dogs_generator will then generate a [DefaultStructureConverter] which
+/// also implements [Copyable] and [Validatable]. The generator will also
+/// generate an implementation of [Builder] for the given type with the suffix
+/// 'Builder' appended to the original class name.
+///  Annotated types must match following conditions:
+/// 1. Have a primary constructor or a secondary constructor
+/// named 'dog' with only positional parameters.
+/// 2. All constructor parameters must be field references. (this.fieldName)
+/// 3. All fields specified in the eligible constructor must be serializable
+/// using a converter or must be of type String, int, double or boolean.
+class Serializable {
+  const Serializable();
+}
+
+/// Marks a class or enum as serializable.
+/// The dogs_generator will then generate a [DefaultStructureConverter] which
+/// also implements [Copyable] and [Validatable]. The generator will also
+/// generate an implementation of [Builder] for the given type with the suffix
+/// 'Builder' appended to the original class name.
+///  Annotated types must match following conditions:
+/// 1. Have a primary constructor or a secondary constructor
+/// named 'dog' with only positional parameters.
+/// 2. All constructor parameters must be field references. (this.fieldName)
+/// 3. All fields specified in the eligible constructor must be serializable
+/// using a converter or must be of type String, int, double or boolean.
+const serializable = Serializable();
+
+/// Manually marks a custom dog converter implementation for linking.
+/// The dogs_generator will then include an instance of this converter.
+class LinkSerializer {
+  const LinkSerializer();
+}
+
+/// Manually marks a custom dog converter implementation for linking.
+/// The dogs_generator will then include an instance of this converter.
+const linkSerializer = LinkSerializer();
+
+/// Overrides the name that will be used by the [GeneratedDogConverter] for this
+/// specific property. By default, the field name will be used.
+class PropertyName {
+  final String name;
+  const PropertyName(this.name);
+}
+
+/// Overrides the serializer that will be used by the [GeneratedDogConverter]
+/// for this specific property. By default, the field will be serialized using
+/// the convert associated with its type.
+class PropertySerializer {
+  final Type type;
+  const PropertySerializer(this.type);
+}
+
+/// Marks a property as polymorphic, meaning its values type can vary.
+class Polymorphic {
+  const Polymorphic();
+}
+
+/// Marks a property as polymorphic, meaning its value's type can vary.
+const polymorphic = Polymorphic();
