08a1f53f1400622d26f28a04d59a80d49814cda9
helight <contact@helight.dev>
2023-08-30 20:59:09 +0200

    fix: add some files I missed

---
+++ packages/dogs_streamed/example/dogs_streamed_example.dart
@@ -0,0 +1,5 @@
+import 'package:dogs_streamed/dogs_streamed.dart';
+
+void main() {
+
+}
---
+++ packages/dogs_streamed/lib/dogs_streamed.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/json.dart';
+export 'src/mode.dart';
+export 'src/primitive_buffer.dart';
+export 'src/schemaless.dart';
