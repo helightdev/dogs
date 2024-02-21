f76dac9ba68156b702ec1bfb102f003c44085c77
helight <contact@helight.dev>
2024-02-21 18:17:11 +0100

    refactor: apply builder only to the root package since
    I previously just selected the wrong target and didn't fix it yet.

--- smoke/test0/lib/dogs.g.dart
+++ smoke/test0/lib/dogs.g.dart
@@ -1,18 +1,24 @@
-import 'package:smoke_test_0/special.dart';
-import 'package:smoke_test_0/models.conv.g.dart';
-import 'package:smoke_test_0/special.conv.g.dart';
-import 'package:smoke_test_0/conformities.conv.g.dart';
-import 'package:smoke_test_0/validation.conv.g.dart';
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, unused_field, unused_import, public_member_api_docs, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
 import 'package:dogs_core/dogs_core.dart';
-import 'package:smoke_test_0/special.dart' as gen0;
-import 'package:smoke_test_0/models.conv.g.dart' as gen1;
-import 'package:smoke_test_0/special.conv.g.dart' as gen2;
 import 'package:smoke_test_0/conformities.conv.g.dart' as gen3;
+import 'package:smoke_test_0/conformities.conv.g.dart';
+import 'package:smoke_test_0/models.conv.g.dart' as gen1;
+import 'package:smoke_test_0/models.conv.g.dart';
+import 'package:smoke_test_0/special.conv.g.dart' as gen2;
+import 'package:smoke_test_0/special.conv.g.dart';
+import 'package:smoke_test_0/special.dart' as gen0;
+import 'package:smoke_test_0/special.dart';
 import 'package:smoke_test_0/validation.conv.g.dart' as gen4;
-export 'package:smoke_test_0/special.dart';
+import 'package:smoke_test_0/validation.conv.g.dart';
+
+export 'package:smoke_test_0/conformities.conv.g.dart';
 export 'package:smoke_test_0/models.conv.g.dart';
 export 'package:smoke_test_0/special.conv.g.dart';
-export 'package:smoke_test_0/conformities.conv.g.dart';
+export 'package:smoke_test_0/special.dart';
 export 'package:smoke_test_0/validation.conv.g.dart';
 
 Future initialiseDogs() async {
